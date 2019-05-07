0acb48f9ee10d7cab3faf5eb6eb1a3baecef8e3e
bolhas <serapinta90@hotmail.com>
2019-05-07 11:32:45 +0100

    Add class House

---
+++ 18Ghosts/House.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _18Ghosts
+{
+    class House
+    {
+        // Properties - Não precisa de se verificar
+        public bool Mirror { get; private set; }
+        public ConsoleColor Color { get; private set; }
+        public Ghost Ghost { get; set; }
+        public Portal Portal { get; private set; }
+        public string Carpet { get; private set; }
+        // Constructors
+        public House(ConsoleColor color, string carpet)
+        {
+            Color = color;
+            Carpet = carpet;
+        }
+
+        public House(bool mirror)
+        {
+            Mirror = mirror;
+        }
+
+        public House(Portal portal)
+        {
+            Portal = portal;
+        }
+
+
+    }
+}
+
