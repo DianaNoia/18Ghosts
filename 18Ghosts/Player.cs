c5f64415665442170e283d628942b3ad822e7ccc
DianaNoia <Diana_noia@hotmail.com>
2019-05-09 10:37:13 +0100

    Changes to Class Player

--- 18Ghosts/Player.cs
+++ 18Ghosts/Player.cs
@@ -6,6 +6,34 @@ namespace _18Ghosts
 {
     class Player
     {
+        // Variables
+        public Ghost[] Ghosts { get; private set; }
+        public Type type;
 
+        //Constructor
+        public Player (Type type, int nghosts)
+        {
+            this.type = type;
+            Ghosts = new Ghost[nghosts];
+        }
+
+        private void InitGhosts()
+        {
+            for(int i=0; i<Ghosts.Length; i++)
+            {
+                if (i < 3)
+                {
+                    Ghosts[i] = new Ghost(type, ConsoleColor.Red);
+                }
+                else if (i < 6)
+                {
+                    Ghosts[i] = new Ghost(type, ConsoleColor.Blue);
+                }
+                else
+                {
+                    Ghosts[i] = new Ghost(type, ConsoleColor.Yellow);
+                }
+            }
+        }
     }
 }
